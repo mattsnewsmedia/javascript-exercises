3da9636297e0fc1cc50e96ee0d3535a157b9ae8d
mattsnewsmedia <matt.marshall5000@gmail.com>
2025-01-03 11:54:28 -0800

    Finished exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let reverse = string;
+    let arrayReverse = Array.from(reverse);
+    arrayReverse.reverse();
+    reverse = arrayReverse.join("");
+    return reverse;
+}
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
