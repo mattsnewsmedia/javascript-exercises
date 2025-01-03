b778a03fecd5e9b6be6fcd7451dd6192e898a7a4
mattsnewsmedia <matt.marshall5000@gmail.com>
2025-01-03 11:53:57 -0800

    Finsihed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeat = string;
+    let repeat2 = "";
+    if(number < 0){
+        return "ERROR";
+    }
+    else{
+for(let i = 0; i < number; i++){
+    repeat2 = repeat2.concat(repeat);
+}
+    }
+    return repeat2;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
