0f7833e5a80eb5759776c4b3bf3f93fefaa7b36f
mattsnewsmedia <matt.marshall5000@gmail.com>
2025-01-03 11:55:54 -0800

    Finished exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year%4 == 0 && year%100 != 0){
+        return true;
+    }    
+    else if(year%100 == 0 && year%400 == 0){
+        return true;
+    }
+    else(year%100 == 0 && year%400 != 0)
+        return false;
 };
 
+
+console.log(leapYears(40));
 // Do not edit below this line
 module.exports = leapYears;
