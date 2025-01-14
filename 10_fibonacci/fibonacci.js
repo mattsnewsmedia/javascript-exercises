a7781f1ad944d6a3539b34cf26b4930df3b12465
mattsnewsmedia <matt.marshall5000@gmail.com>
2025-01-14 14:40:25 -0800

    Finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let firstNumber = 1;
+    let secondNumber = 0;
+    let thirdNumber = 0
 
+    let count
+    if (typeof num !== 'number') {
+        count = parseInt(num)
+    } else {
+        count = num
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    for(let i = 2; i<=count; i++){
+        thirdNumber = firstNumber + secondNumber;
+        secondNumber = firstNumber;
+        firstNumber = thirdNumber; 
+        
+    }
+    return firstNumber;
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
