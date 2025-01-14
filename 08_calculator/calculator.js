e148605755bea42d694617b2d26c293ac3b6d6a7
mattsnewsmedia <matt.marshall5000@gmail.com>
2025-01-13 19:50:28 -0800

    finished calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length < 1){
+    return 0;
+  }
+  else
+	return arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  });
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = a;
+  if(a == 0){
+    return 1;
+  }
+	while(a > 0){
+    if(a == 1){
+      return total
+    }
+    total = total * (a - 1);
+    a--
+  }
 };
 
 // Do not edit below this line
